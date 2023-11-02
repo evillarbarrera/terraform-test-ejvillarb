9446cb2dab8a3b7da24bfabeedfe72292cc371de
Emmanuel Villar <e.villarbarrera@gmail.com>
2023-11-02 12:47:41 -0300

    prueba cambio

--- vpc.tf
+++ vpc.tf
@@ -75,7 +75,6 @@ resource "aws_security_group" "security_public_instance" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
